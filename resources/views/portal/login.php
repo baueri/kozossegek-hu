0cdbc91b493341510c3147df4c9815bc2082a087
Ivan Bauer <birkaivan@gmail.com>
2020-09-30 03:14:53 +0200

    login header

--- resources/views/portal/login.php
+++ resources/views/portal/login.php
@@ -1,6 +1,10 @@
 <!DOCTYPE>
 <html lang="hu">
     <head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>kozossegek.hu - Belépés</title>
         <style>
             @import url(https://fonts.googleapis.com/css?family=Roboto:300);
