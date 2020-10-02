e6f576b44b42f3fef73ba6a31004a3ee7e3c6229
Bauer Iván <bauer.ivan@zengo.eu>
2020-10-03 00:08:09 +0200

    .env fájlok élesre és demo-ra

---
+++ _env/.env_eles.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'ENVIRONMENT' => 'production',
+    'DEBUG' => false,
+
+    'DB_HOST' => 'mysql.nethely.hu',
+    'DB_USER' => 'kozossegek_eles',
+    'DB_PASSWORD' => 'tlx0BGzO',
+    'DB_NAME' => 'kozossegek_eles',
+
+    'DB_PORT' => '3306',
+    'BASE_AUTH' => false,
+    'COMING_SOON' => true,
+
+    //EMAIL beállítások
+    'EMAIL_HOST' => 'mail.nethely.hu',
+    'EMAIL_PORT' => '1025',
+    'EMAIL_ADDRESS' => 'noreply@kozossegek.hu',
+    'EMAIL_PASSWORD' => '***REMOVED***',
+    'EMAIL_SSL' => 'STARTTLS',
+
+    'ERROR_EMAIL' => 'error@kozossegek.hu'
+
+];
