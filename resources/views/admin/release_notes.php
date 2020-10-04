09fad53b332bd25704c0fd826e0c779e444a8241
Ivan Bauer <birkaivan@gmail.com>
2020-10-04 03:07:45 +0200

    verzióinfó frissítés, verziófrissítés

--- resources/views/admin/release_notes.php
+++ resources/views/admin/release_notes.php
@@ -1,5 +1,16 @@
 @title('Verzióinformáció')
 @extends('admin')
+
+<h3>v0.0.2 pre-alpha</h3>
+<ul>
+    <li>Javítások</li>
+    <li>Címke tábla migráció, feltöltés adattal</li>
+    <li>Lelkiségi mozagalmas tábla migráció, feltöltés adattal</li>
+    <li>Címkék, mozgalmak admin felület fejlesztése</li>
+    <li>Közösségek tábla szerkezetének átalakítása, nézettábla átalakítás</li>
+</ul>
+
+
 <h3>v0.0.1 pre-alpha</h3>
 <ul>
     <li>Keretrendszer fejlesztése</li>
