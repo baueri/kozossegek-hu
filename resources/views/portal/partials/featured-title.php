5d6dacd8cd0b9acd246a88a051106db88f0915ec
Iván <birkaivan@gmail.com>
2020-10-31 23:22:06 +0100

    KOZ-10 kisebb design fixek

---
+++ resources/views/portal/partials/featured-title.php
@@ -0,0 +1,8 @@
+<div class="featured">
+    <div class="container justify-content-center text-">
+        <div class="featured-content">
+            <h1 class='h2'>{{ $title }}</h1>
+        </div>
+    </div>
+    <div style="opacity: 1.0;" class="featured-overlay"></div>
+</div>
