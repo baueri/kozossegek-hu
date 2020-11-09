4aefabb5eb923a19e63218fc30609669ea5f82ed
Iván <birkaivan@gmail.com>
2020-11-09 01:17:27 +0100

    főleg design beli módosítások

--- resources/views/admin.php
+++ resources/views/admin.php
@@ -1,9 +1,9 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
 <head>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
-    <meta charset="UTF-8">
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>kozossegek.hu - ADMIN</title>
 
