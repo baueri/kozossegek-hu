f7b484d81dd27f1e855ab85d78f3a72860c983dc
Iván <birkaivan@gmail.com>
2020-12-27 16:53:20 +0100

    featured title gyors fix

--- app/Http/Components/FeaturedTitle.php
+++ app/Http/Components/FeaturedTitle.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace App\Http\Components;
-
 
 class FeaturedTitle
 {
     public function render(string $title)
     {
-        view('portal.partials.featured-title', compact('title'));
+        return view('portal.partials.featured-title', compact('title'));
     }
 }
