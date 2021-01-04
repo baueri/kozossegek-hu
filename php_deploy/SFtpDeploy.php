75e27b7a7e2187fcf5986d64f8f3d45091a3d056
Iván <birkaivan@gmail.com>
2021-01-04 21:54:38 +0100

    php deploy

--- php_deploy/SFtpDeploy.php
+++ php_deploy/SFtpDeploy.php
@@ -1,17 +1,22 @@
 <?php
 
-class FtpDeploy
+class SFtpDeploy
 {
-    private $configuration;
+    private array $configuration;
 
     public function __construct(array $configuration)
     {
         $this->configuration = $configuration;
     }
 
-    private function connect() {
-        $connection = ftp_connect($configuration['domain'], $configuration['port'] ?? 22);
-
+    /**
+     * @return resource
+     * @throws Exception
+     */
+    private function connect()
+    {
+        $connection = ssh2_connect($this->configuration['domain'], $this->configuration['port'] ?? 22);
+        var_dump($connection);exit;
         if (!$connection) {
             throw new \Exception('Could not connect to ftp');
         }
@@ -19,25 +24,28 @@ class FtpDeploy
         return $connection;
     }
 
-    private function close($connection) {
+    private function close($connection)
+    {
         ftp_close($connection);
     }
 
     private function login()
     {
         $connection = $this->connect();
-
-
     }
 
+    /**
+     * @param $filename
+     * @throws Exception
+     */
     public function touch($filename)
     {
+        $this->connect();
         print_r($filename);
         exit(1);
     }
 
     public function __destruct()
     {
-
     }
 }
