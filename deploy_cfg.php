58168f7b4f6175b1922cd1e0dcb066badce0c9d2
Bauer Iván <bauer.ivan@zengo.eu>
2021-01-04 18:35:26 +0100

    deploy fejlesztés

---
+++ deploy_cfg.php
@@ -0,0 +1,16 @@
+<?php return [
+    'production' => [
+        'host' => [
+            'domain' => 'ftp.nhely.hu',
+            'user' => 'user',
+            'password' => 'password'
+        ]
+    ],
+    'development' => [
+        'host' => [
+            'domain' => 'ftp.nhely.hu',
+            'user' => 'user',
+            'password' => 'password'
+        ]
+    ]
+];
