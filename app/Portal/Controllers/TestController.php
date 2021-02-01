2cfb0280252b604f52818cab4334942dbd89c5a0
Iván <birkaivan@gmail.com>
2021-02-01 21:42:21 +0100

    teszter link csere, event log javítás

--- app/Portal/Controllers/TestController.php
+++ app/Portal/Controllers/TestController.php
@@ -2,20 +2,12 @@
 
 namespace App\Portal\Controllers;
 
+use Framework\Support\StringHelper;
+
 class TestController
 {
     public function test()
     {
-        echo '<pre>';
-        print session_id() . '<br>';
-
-        session_destroy();
-
-        session_id(session_create_id());
-
-        session_start();
-
-
-        print session_id();
+        print StringHelper::generateRandomStringFrom('ABCDEFGHJKLMNPRSTUVWXYZ123456789', 5);
     }
 }
