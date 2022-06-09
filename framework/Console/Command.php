d7dc8af06f4dbdb72e266e0c7450c01a71eb5f67
Bauer Iván <bauer.ivan@zengo.eu>
2022-06-09 12:42:57 +0200

    fix daily statistics log sync error

--- framework/Console/Command.php
+++ framework/Console/Command.php
@@ -4,7 +4,10 @@ namespace Framework\Console;
 
 interface Command
 {
+    public const SUCCESS = 0;
+    public const FAILURE = 1;
+
     public static function signature(): string;
 
-    public function handle(): void;
+    public function handle();
 }
