27f609fdccfd4c230c3c19edcd9cb7321d3500b2
Bauer Iván <bauer.ivan@zengo.eu>
2022-06-09 12:42:57 +0200

    fix daily statistics log sync error

--- app/Models/CityStat.php
+++ app/Models/CityStat.php
@@ -4,6 +4,12 @@ namespace App\Models;
 
 use Framework\Model\Entity;
 
+/**
+ * @property string $city
+ * @property int $search_count
+ * @property int $opened_groups_count
+ * @property int $contacted_groups_count
+ */
 class CityStat extends Entity
 {
 
--- framework/Dispatcher/Dispatcher.php
+++ framework/Dispatcher/Dispatcher.php
@@ -4,7 +4,7 @@ namespace Framework\Dispatcher;
 
 interface Dispatcher
 {
-    public function dispatch(): void;
+    public function dispatch();
 
     /**
      * @param mixed $e
