14a7c47e83958a3f6ba3da654d90afd3f8757bd5
Bauer Iván <bauer.ivan@zengo.eu>
2022-10-07 11:26:28 +0200

    institute groups route change, remove widgets

--- framework/Model/ModelNotFoundException.php
+++ framework/Model/ModelNotFoundException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class ModelNotFoundException extends \Exception
 {
-    public function __construct($message = "", $code = 500, Throwable $previous = null)
+    public function __construct($message = "", $code = 404, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
