e0c1111d23159a39d7b8c07e1216210e9a6c56c3
Bauer Iván <bauer.ivan@zengo.eu>
2023-06-17 19:52:21 +0200

    dockerize

--- db/seeds/TagSeeder.php
+++ db/seeds/TagSeeder.php
@@ -7,7 +7,7 @@ class TagSeeder extends AbstractSeed
 {
     public function run()
     {
-        $tags = collect_file(ROOT . 'db/sources/tags.txt');
+        $tags = collect_file(ROOT . DS . 'db' . DS . 'sources' . DS . 'tags.txt');
 
         foreach ($tags as $tag) {
             $slug = StringHelper::slugify($tag);
---
+++ docker/app/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ./cache && mkdir -p ${STORAGE_PATH}
+chown www-data:www-data -R /app/cache && chown www-data:www-data -R ${STORAGE_PATH}
+
+service apache2 start
+
+while true
+do
+   sleep 1
+done
