cb5313d67999c455cba7608ab8730a89ae79112d
Ivan Bauer <ivan.bauer90@gmail.com>
2024-01-28 21:20:27 +0100

    lelkisegi mozgalom tag fix

--- resources/views/portal/spiritual_movement/view.php
+++ resources/views/portal/spiritual_movement/view.php
@@ -26,7 +26,7 @@
                     <div class="card-body">
                         <p class="text-center">
                             @foreach($group->tags as $tag)
-                            <span class="tag-img tag-{{ $tag['tag'] }}" title="{{ $tag['tag_name'] }}" aria-label="{{ $tag['tag_name'] }}"></span>
+                            <span class="tag-img tag-{{ $tag->tag }}" title="{{ $tag->translate() }}" aria-label="{{ $tag->translate() }}"></span>
                             @endforeach
                         </p>
                         <div>{{ $group->name }}</div>
